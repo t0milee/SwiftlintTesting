a37b6c97d2cd001b1ea441a968a85cb921b677a9
Tomislav Jankovic <jankovic.tm@gmail.com>
2017-03-14 13:26:00 +0100

    testing jenkins

--- SwiftlintTesting/AppDelegate.swift
+++ SwiftlintTesting/AppDelegate.swift
@@ -25,6 +25,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to
         // pause the game.
+        
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
